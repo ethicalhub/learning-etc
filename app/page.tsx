d81a8d5d7a25559eadcd57fd3c186202e9c91319
ethicalhub <yasiransari.elith@gmail.com>
2025-01-14 07:26:21 +0530

    feat(setup): simple links at home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
+import Link from "next/link";
 import React from "react";
 
 const Page = () => {
-  return <div className="bg-red-500">Base for documentation website</div>;
+  return (
+    <section className="flex min-h-screen items-center justify-center bg-lightBg">
+      <div className="flex flex-wrap items-center gap-4">
+        <Link
+          className="cursor-pointer rounded-lg bg-primary px-4 py-2 text-white"
+          href={"/notes/about"}
+        >
+          Read Notes
+        </Link>
+        <Link
+          className="cursor-pointer rounded-lg bg-secondary px-4 py-2 text-white"
+          href={"https://yasir-etc.medium.com/"}
+        >
+          Visit Blog
+        </Link>
+        <Link
+          className="cursor-pointer rounded-lg bg-dark px-4 py-2 text-white"
+          href={"https://www.linkedin.com/in/yasir-etc/"}
+        >
+          Visit Portfolio
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Page;
