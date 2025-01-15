8e0cb53b26642d467a15ec83f0a0e84f29de52e2
ethicalhub <yasiransari.elith@gmail.com>
2025-01-15 19:42:41 +0530

    fix(bug): bug for name change in dsa

--- pages/notes/dsa/dsainaction/_meta.js
+++ pages/notes/dsa/dsainaction/_meta.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   quizzes: "Quiz",
-  miniproject: "Mini Project Challenge",
+  miniprogram: "Mini Program Challenge",
   interviewpreparation: "Interview Questions",
 };
